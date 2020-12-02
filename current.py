6e6c02b3349c2a6dbc2baee58aff05ad079f06be
chanisaramint <chanisaramint@gmail.com>
2020-12-02 11:40:51 +0700

    clean code

--- current.py
+++ current.py
@@ -2,6 +2,7 @@ from db_config import mysql  # import sql
 
 def cal_fines(fines):
     mycursor = mysql.connection.cursor()
+    sql = "update test_log set fines = %s where id = 0"
     sql_parking = "update parking_log set fines = %s where license_plate = 'กข45678'"
     val = (fines,)
     mycursor.execute(sql_parking, val)
@@ -11,6 +12,7 @@ def cal_fines(fines):
 
 def cal_discount(discount):
     mycursor = mysql.connection.cursor()
+    sql = "update test_log set discount = %s where id = 0"
     sql_parking = "update parking_log set discount = %s where license_plate = 'กข45678'"
     val = (discount,)
     mycursor.execute(sql_parking, val)
@@ -20,6 +22,7 @@ def cal_discount(discount):
     
 def cal_receieve(receieve):
     mycursor = mysql.connection.cursor()
+    sql = "update test_log set discount = %s where id = 0"
     sql_parking = "update parking_log set earn = %s where license_plate = 'กข45678'"
     val = (receieve,)
     mycursor.execute(sql_parking, val)
@@ -29,6 +32,7 @@ def cal_receieve(receieve):
         
 def cal_changes(changes):
     mycursor = mysql.connection.cursor()
+    sql = "update test_log set discount = %s where id = 0"
     sql_parking = "update parking_log set changes = %s where license_plate = 'กข45678'"
     val = (changes,)
     mycursor.execute(sql_parking, val)
